6aa4aac6d6dd7fedca3cc89c86d1a83906905d26
HOME <coderiding9060@gmail.com>
2021-10-22 19:27:42 +0900

    Init Commit

---
+++ src/test/java/com/cnw/MyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
